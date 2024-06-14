f3439010e2d3df53248e85477635364ae84ddd95
Diogo Eugênio <diogodeeugenio@gmail.com>
2024-06-13 22:22:02 -0300

    commit de ultima hora

--- src/diagramaIphoneImplementado/IPhone.java
+++ src/diagramaIphoneImplementado/IPhone.java
@@ -1,5 +1,32 @@
 package diagramaIphoneImplementado;
 
-public class IPhone {
+public class IPhone implements ReprodutorMusical, AparelhoTelefone, NavegadorInternet{
 
+	@Override
+	public void exibirPagina(String url) {}
+
+	@Override
+	public void adicionarNovaAba() {}
+
+	@Override
+	public void atualizarPagina() {}
+
+	@Override
+	public void ligar(String numero) {}
+
+	@Override
+	public void atender() {}
+
+	@Override
+	public void iniciarCorreioVoz() {}
+
+	@Override
+	public void tocar() {}
+
+	@Override
+	public void pausar() {}
+
+	@Override
+	public void selecionarMusica() {}
+	
 }
